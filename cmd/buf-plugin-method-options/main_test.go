c8a6e91caa8ea8cc93f434e1112109987006d5e0
Toni Reina <toni.reina@qdrant.com>
2025-06-10 17:46:37 +0200

    Update the `method-options` plugin making it configurable (#8)
    * Update the `method-options` plugin making it configurable
    
    With this change it is possible to configure the list of required method
    options. For our internal api, we can exclude the validation of
    `google.api.http` because we don't use it.
    
    * Fix linting issue

--- cmd/buf-plugin-method-options/main_test.go
+++ cmd/buf-plugin-method-options/main_test.go
@@ -62,3 +62,60 @@ func TestSimpleFailure(t *testing.T) {
 		},
 	}.Run(t)
 }
+
+func TestSimpleFailureWithOption(t *testing.T) {
+	t.Parallel()
+	checktest.CheckTest{
+		Request: &checktest.RequestSpec{
+			Files: &checktest.ProtoFileSpec{
+				DirPaths:  []string{"testdata/simple_failure"},
+				FilePaths: []string{"simple.proto"},
+			},
+			Options: map[string]any{
+				methodOptionsOptionKey: []string{"qdrant.cloud.common.v1.permissions", "unknown.extension"},
+			},
+		},
+		Spec: spec,
+		ExpectedAnnotations: []checktest.ExpectedAnnotation{
+			{
+				RuleID:  methodOptionsRuleID,
+				Message: "extension key \"unknown.extension\" does not exist",
+			},
+			{
+				RuleID:  methodOptionsRuleID,
+				Message: "Method \"simple.GreeterService.HelloWorld\" does not define the \"qdrant.cloud.common.v1.permissions\" option",
+				FileLocation: &checktest.ExpectedFileLocation{
+					FileName:    "simple.proto",
+					StartLine:   9,
+					StartColumn: 4,
+					EndLine:     12,
+					EndColumn:   5,
+				},
+			},
+		},
+	}.Run(t)
+
+}
+
+func TestSimpleFailureWithOptionWrongKey(t *testing.T) {
+	t.Parallel()
+	checktest.CheckTest{
+		Request: &checktest.RequestSpec{
+			Files: &checktest.ProtoFileSpec{
+				DirPaths:  []string{"testdata/simple_failure"},
+				FilePaths: []string{"simple.proto"},
+			},
+			Options: map[string]any{
+				methodOptionsOptionKey: []string{"unknown.extension"},
+			},
+		},
+		Spec: spec,
+		ExpectedAnnotations: []checktest.ExpectedAnnotation{
+			{
+				RuleID:  methodOptionsRuleID,
+				Message: "extension key \"unknown.extension\" does not exist",
+			},
+		},
+	}.Run(t)
+
+}
